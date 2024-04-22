94236c7ebd77bd6ecf77a78a791c584aef7a95c8
DavidThompson <David121801.dt@gmail.com>
2024-04-22 19:15:33 -0400

    syntax error

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const initialCards = {
-  {
+
     title: "Yosemite Valley",
     link; "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   }
@@ -22,6 +22,5 @@ const initialCards = {
   {
     title: "Lago di Braies",
     link; "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  }
-};
+  };
 console.log(initialCards);
